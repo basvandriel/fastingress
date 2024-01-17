adee4aea0900c11664b622fff87670e476c4b7cc
Bas van Driel <contact@basvandriel.nl>
2024-01-17 08:39:45 +0100

    add todos

--- src/service_resolver.rs
+++ src/service_resolver.rs
@@ -1,6 +1,11 @@
 use std::net::SocketAddr;
 
 pub fn resolve_service_ip(name: &str) -> SocketAddr {
+    // TODO find and define Kubernetes cluster ip + port (defaults to https://localhost:6443)
+    // TODO find how to access the local kubernetes cluster IP's.
+    // TODO authenticate to the Kubernetes API
+    // TODO find service in Kubernetes API and retrieve cluster IP
+    // TODO test IP connectivity in a later integration test
     println!("Resolving {name}");
     let ip = SocketAddr::from(([127, 0, 0, 1], 8000));
 
