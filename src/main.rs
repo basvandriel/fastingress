6053a9bdfa9fe3b82ae304f5b783f4d76e638e4b
Bas van Driel <contact@basvandriel.nl>
2024-01-19 14:47:44 +0100

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::error::Error;
 use std::net::SocketAddr;
 
 use fastingress::accept_connection;
@@ -5,7 +6,7 @@ use fastingress::constants::DEFAULT_LISTENING_PORT;
 use tokio::net::TcpListener;
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
     let addr = SocketAddr::from(([127, 0, 0, 1], DEFAULT_LISTENING_PORT));
     let listener = TcpListener::bind(addr).await?;
 
