ced82ee10d0e6da700d5a56574fc54c4b9d8fb3b
Bas van Driel <contact@basvandriel.nl>
2024-01-24 21:16:54 +0100

    work

--- src/service_resolver.rs
+++ src/service_resolver.rs
@@ -1,4 +1,4 @@
-use std::{env, net::SocketAddr};
+use std::env;
 
 use hyper::Uri;
 
@@ -6,10 +6,6 @@ use crate::constants::{DEFAULT_CLUSTER_IP, DEFAULT_PROXY_PORT};
 
 pub fn running_in_kubernetes_cluster() -> bool {
     return env::var("KUBERNETES_SERVICE_HOST").is_ok();
-}
-
-fn should_proxy() -> bool {
-    return running_in_kubernetes_cluster();
 }
 
 pub struct KubeServiceLocation {
@@ -41,21 +37,4 @@ pub fn build_service_proxy_url(service_loc: &KubeServiceLocation, original_uri: 
         service_url += &path_and_query.to_string();
     }
     return service_url.parse::<Uri>().unwrap();
-}
-
-pub fn resolve_service_ip(name: &str) -> SocketAddr {
-    if !should_proxy() {
-        // TODO
-    }
-    println!("Resolving {name}");
-    // let z = x +=
-    // let api_url = "http://" + DEFAULT_CLUSTER_IP + ":";
-    // TODO find and define Kubernetes cluster ip + port
-    // defaults to https://localhost:6443 or  https://kubernetes.docker.internal:6443 for docker desktop
-    //
-    // TODO find service in Kubernetes API and retrieve cluster IP
-    // TODO test IP connectivity in a later integration test
-    let ip = SocketAddr::from(([127, 0, 0, 1], 8000));
-
-    return ip;
 }
--- tests/test_ip_resolver.rs
+++ tests/test_ip_resolver.rs
@@ -1,18 +1,5 @@
-use std::net::SocketAddr;
-
-use fastingress::service_resolver::{
-    build_service_proxy_url, resolve_service_ip, KubeServiceLocation,
-};
+use fastingress::service_resolver::{build_service_proxy_url, KubeServiceLocation};
 use hyper::Uri;
-
-#[test]
-fn it_adds_two() {
-    let x: SocketAddr = resolve_service_ip("nginx-service");
-
-    assert_eq!(x.port(), 8000);
-    assert_eq!(x.ip().to_string(), "127.0.0.1");
-    assert_eq!(x.is_ipv4(), true);
-}
 
 #[test]
 fn it_finds_correct_service() {
