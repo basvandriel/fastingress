8588d4eb429ce367e5f663e07ee5c092454488f3
Bas van Driel <contact@basvandriel.nl>
2024-01-25 12:05:03 +0100

    work

--- src/ingress.rs
+++ src/ingress.rs
@@ -1,12 +1,8 @@
 use hyper::body::Incoming;
-use hyper::service::Service;
 use hyper::Request;
 use hyper::Uri;
-use std::future::Future;
-use std::pin::Pin;
 use std::time::Instant;
 
-use crate::logger::log_request;
 use crate::logger::Logger;
 use crate::proxy::proxy_response;
 use crate::proxy::R;
@@ -22,7 +18,17 @@ type RQ = Request<Incoming>;
 pub struct IngressRequestHandler;
 
 impl IngressRequestHandler {
-    fn build_url_resolver(original_uri: Uri) -> Box<dyn UrlResolver> {
+    fn log_request<T>(&self, request: Request<T>, duration_ms: u128) {
+        let method = request.method();
+        let path = request.uri().path();
+
+        let message = format!("{} \"{}\" - took {}ms", method, path, duration_ms);
+
+        let logger = Logger {};
+        logger.info(&message);
+    }
+
+    fn build_url_resolver(&self, original_uri: Uri) -> Box<dyn UrlResolver> {
         if running_in_kubernetes_cluster() {
             return Box::new(InClusterServiceURLResolver {
                 original_url: original_uri,
@@ -33,42 +39,30 @@ impl IngressRequestHandler {
         });
     }
 
-    async fn resolve_url(original_uri: &Uri) -> Uri {
+    async fn resolve_url(&self, original_uri: &Uri) -> Uri {
         let loc = KubeServiceLocation {
             namespace: String::from("default"),
             name: String::from("nginx-service"),
             port: 80,
         };
-        let url = Self::build_url_resolver(original_uri.clone()).resolve(&loc);
+        let url = self.build_url_resolver(original_uri.clone()).resolve(&loc);
 
         return url.expect("URI should be there");
     }
 
-    async fn proxy_to_service(request: RQ) -> Result<R, ErrorType> {
+    pub async fn proxy_to_service(&self, request: RQ) -> Result<R, ErrorType> {
         let logger: Logger = Logger {};
 
         let start = Instant::now();
-        let url = Self::resolve_url(request.uri()).await;
+        let url = self.resolve_url(request.uri()).await;
         logger.info(format!("Routing to URI: {}", url).as_str());
 
         // TODO use everything from original request (method, body, ...)
         let result = proxy_response(url).await?;
 
-        log_request(request, start.elapsed().as_millis());
-
+        self.log_request(request, start.elapsed().as_millis());
         return Ok(result);
     }
 }
 
 pub type ErrorType = Box<dyn std::error::Error + Send + Sync>;
-
-impl Service<RQ> for IngressRequestHandler {
-    type Response = R;
-    type Error = ErrorType;
-    type Future = Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>> + Send>>;
-
-    fn call(&self, req: RQ) -> Self::Future {
-        let res = Self::proxy_to_service(req);
-        Box::pin(res)
-    }
-}
--- src/lib.rs
+++ src/lib.rs
@@ -7,20 +7,29 @@ pub mod proxy;
 pub mod service_resolver;
 pub mod uri_resolver;
 pub mod utils;
-use hyper::server::conn::http1::Builder as HTTPBuilder;
 
+use hyper::body::Incoming;
+use hyper::server::conn::http1::Builder as HTTPBuilder;
+use hyper::Request;
+
+use crate::proxy::R;
+use hyper::service::service_fn;
 use hyper_util::rt::TokioIo;
-use ingress::IngressRequestHandler;
+use ingress::{ErrorType, IngressRequestHandler};
 
 use tokio::net::TcpListener;
 
 use tokio::task::spawn;
 
+async fn hello(req: Request<Incoming>) -> Result<R, ErrorType> {
+    return IngressRequestHandler.proxy_to_service(req).await;
+}
+
 pub async fn accept_connection(listener: &TcpListener) -> () {
     let (stream, _) = listener.accept().await.expect("No");
     let io = TokioIo::new(stream);
 
-    let service = IngressRequestHandler {};
+    let service = service_fn(hello);
 
     spawn(async move {
         if let Err(err) = HTTPBuilder::new().serve_connection(io, service).await {
