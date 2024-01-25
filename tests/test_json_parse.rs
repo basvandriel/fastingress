56076482322ee7c4be30f2ca8411451baac83260
Bas van Driel <contact@basvandriel.nl>
2024-01-25 16:52:00 +0100

    direct parse

--- tests/test_json_parse.rs
+++ tests/test_json_parse.rs
@@ -1,5 +1,6 @@
 use fs::File;
 use serde::{Deserialize, Serialize};
+use serde_json::Value;
 use std::{env, fs, io::BufReader, path::PathBuf};
 
 fn get_project_root() -> PathBuf {
@@ -52,32 +53,59 @@ struct KubeAPIObjectSpec {
     rules: Vec<KubeAPIObjectSpecRule>,
 }
 
-#[derive(Serialize, Deserialize)]
-struct KubeAPIObject {
-    // ingress_type: String,
-    kind: String,
-
-    #[serde(rename = "apiVersion")]
-    api_version: String,
-    spec: KubeAPIObjectSpec,
-}
-
-#[derive(Serialize, Deserialize)]
-struct Foo {
-    object: KubeAPIObject,
-}
-
 #[test]
-fn it_shouldwork() {
+fn it_shouldwork_strictly() {
     let mut jsonpath = get_kubernetes_path();
     jsonpath.push("sample_ingress_api_response.json");
 
-    let mut file = File::open(jsonpath).expect("file should open");
-
-    let mut data = String::new();
+    let file = File::open(jsonpath).expect("file should open");
     let reader = BufReader::new(file);
 
+    #[derive(Serialize, Deserialize)]
+    struct KubeAPIObject {
+        // ingress_type: String,
+        kind: String,
+
+        #[serde(rename = "apiVersion")]
+        api_version: String,
+        spec: KubeAPIObjectSpec,
+    }
+
+    #[derive(Serialize, Deserialize)]
+    struct Foo {
+        object: KubeAPIObject,
+    }
     let u: Foo = serde_json::from_reader(reader).expect("Should parse");
 
-    println!("hi")
+    assert_eq!(u.object.spec.rules[0].http.paths[0].path, "/");
+    assert_eq!(u.object.spec.rules[0].http.paths[0].path_type, "Prefix");
+    assert_eq!(
+        u.object.spec.rules[0].http.paths[0].backend.service.name,
+        "nginx-service"
+    );
+    assert_eq!(
+        u.object.spec.rules[0].http.paths[0]
+            .backend
+            .service
+            .port
+            .number,
+        80
+    );
+}
+
+#[test]
+fn it_shouldwork_justpath() {
+    let mut jsonpath = get_kubernetes_path();
+    jsonpath.push("sample_ingress_api_response.json");
+
+    let file = File::open(jsonpath).expect("file should open");
+    let reader = BufReader::new(file);
+
+    let root: Value = serde_json::from_reader(reader).expect("Should parse");
+    let entries = &root["object"].as_object().unwrap()["spec"];
+
+    let spec: KubeAPIObjectSpec =
+        serde_json::from_value(entries.to_owned()).expect("JSON should parse");
+
+    assert_eq!(spec.rules[0].http.paths[0].path, "/");
 }
