87fb21e8dad7c24feb9aad3de7f68b438a426db5
Bas van Driel <contact@basvandriel.nl>
2024-03-06 12:43:10 +0100

    rm unused

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 pub mod api_resolver;
 pub mod constants;
 pub mod ingress;
-pub mod kube_api_structs;
 pub mod logger;
 pub mod paths;
 pub mod proxy;
@@ -11,8 +10,6 @@ pub mod request_handler;
 pub mod utils;
 
 pub mod types;
-
-pub mod ingress_resource_json_parser;
 
 pub mod api_watcher;
 pub mod ingress_resource_resolver;
